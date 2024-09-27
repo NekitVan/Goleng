ab6e4c605519fd36ec6166226bd4a5bed46e2f80
Nik <kolonkovn@gmail.com>
2024-09-27 17:05:32 +0500

    quickSort

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	Task2()
+	Task3()
+	Task4()
 }
 
 // Task2
@@ -17,4 +19,39 @@ func Task2() {
 	fmt.Println(name)
 	fmt.Println(age)
 
+}
+
+// Task3
+func Task3() {
+	var number [5]int = [5]int{1, 2, 3, 4, 5}
+	//срез
+	var nomber2 = [...]int{1, 2, 3}
+	fmt.Println(number)
+	fmt.Println(nomber2[1])
+
+}
+func Task4() {
+	array := []int{1, 3, 5, 7, 1, 5, 7, 21, 1, 1, 0, 3, 5}
+	sortedArr := sort(array)
+	fmt.Println("массив:", array)
+	fmt.Println("Отсортированный массив:", sortedArr)
+}
+
+func sort(array []int) []int {
+	if len(array) <= 1 {
+		return array
+	}
+	pivot := array[0]
+
+	var min []int
+	var max []int
+
+	for _, v := range array[1:] {
+		if v < pivot {
+			min = append(min, v)
+		} else {
+			max = append(max, v)
+		}
+	}
+	return append(append(sort(min), pivot), sort(max)...)
 }
